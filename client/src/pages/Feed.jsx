045a3499287c757f22b4da03ec0a5fe65084d630
harsh <harshguptahb055@gmail.com>
2025-12-11 02:12:53 +0530

    started implementing feed page

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets';
+import Loading from '../components/Loading';
 
 const Feed = () => {
-  return (
-    <div></div>
-  )
+  const [feeds, setfeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchFeeds = async () => {
+    setfeeds(dummyPostsData);
+  }
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+      {/* Stories and post list */}
+      <div>
+        <h1>Stories here</h1>
+        <div>
+          list of posts
+        </div>
+      </div>
+
+      {/* right sidebar */}
+      <div>
+
+      </div>
+    </div>
+  ) : <Loading/>
 }
 
 export default Feed
