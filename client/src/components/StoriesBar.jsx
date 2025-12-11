cdd295a8abbeea85f4ecad4bddd6a596c0ee8bcf
harsh <harshguptahb055@gmail.com>
2025-12-12 03:08:56 +0530

    started implementing stories component

---
+++ client/src/components/StoriesBar.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const StoriesBar = () => {
+    const [stories, setStories] = useState([]);
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default StoriesBar
