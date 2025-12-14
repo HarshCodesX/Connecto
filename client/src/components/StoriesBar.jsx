a02fca5bba06c59df592279ec8079b281d55714a
harsh <harshguptahb055@gmail.com>
2025-12-14 21:02:01 +0530

    started implementing view story modal

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -3,6 +3,7 @@ import { dummyStoriesData } from "../assets/assets";
 import { Plus } from "lucide-react";
 import moment from "moment";
 import StoryModal from "./StoryModal";
+import StoryViewer from "./StoryViewer";
 
 const StoriesBar = () => {
     const [stories, setStories] = useState([]);
@@ -29,7 +30,7 @@ const StoriesBar = () => {
             {/* Story Card */}
             {
                 stories.map((story, index) => (
-                    <div className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`} key={index}>
+                    <div onClick={() => setViewStory(story)} className={`relative rounded-lg shadow min-w-30 max-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`} key={index}>
                         <img className="absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow" src={story.user.profile_picture} alt="user-profile" />
                         <p className="absolute top-18 left-3 text-white/60 text-sm truncate max-w-24">{story.content}</p>
                         <p className="text-white absolute bottom-1 right-2 z-10 text-xs">{moment(story.createdAt).fromNow()}</p>
@@ -52,6 +53,11 @@ const StoriesBar = () => {
         {/* Add Story Modal */}
         {
             showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories}/>
+        }
+
+        {/* View Story Modal */}
+        {
+            viewStory && <StoryViewer viewStory={viewStory} setViewStory={setViewStory} />
         }
     </div>
   )
---
+++ client/src/components/StoryViewer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StoryViewer = ({viewStory, setViewStory}) => {
+  return (
+    <div className='fixed inset-0 h-screen bg-black bg-opacity-90 z-110 flex items-center justify-center' style={{backgroundColor: viewStory.media_type === "text" ? viewStory.background_color : '#000000'}}>
+
+    {/* Progress Bar */}
+    </div>
+  )
+}
+
+export default StoryViewer
