1f7c836d4d7ce14075f37e8539828511a9fa9ef6
harsh <harshguptahb055@gmail.com>
2025-12-25 04:57:02 +0530

    continued profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import { dummyPostsData, dummyUserData } from '../assets/assets';
 import Loading from '../components/loading';
+import UserProfileInfo from '../components/UserProfileInfo';
 
 const Profile = () => {
   const {profileId} = useParams();
@@ -30,6 +31,7 @@ const Profile = () => {
           </div>
 
           {/* User Info */} 
+          <UserProfileInfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit} />
         </div>
       </div>
     </div>
