d83502b5de0474aba55480f6a4d2a7c31ef4d58c
harsh <harshguptahb055@gmail.com>
2025-12-26 16:31:34 +0530

    implemented posts on user profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import {useParams} from 'react-router-dom';
 import { dummyPostsData, dummyUserData } from '../assets/assets';
 import Loading from '../components/loading';
 import UserProfileInfo from '../components/UserProfileInfo';
+import PostCard from '../components/PostCard';
 
 const Profile = () => {
   const {profileId} = useParams();
@@ -33,6 +34,26 @@ const Profile = () => {
           {/* User Info */} 
           <UserProfileInfo user={user} posts={posts} profileId={profileId} setShowEdit={setShowEdit} />
         </div>
+
+        {/* Tabs */}
+        <div className='mt-6'>
+          <div className='bg-white rounded-xl shadow p-1 flex max-w-md mx-auto'>
+            {["posts", "media", "likes"].map((tab) => (
+              <button onClick={() => setActiveTab(tab)} key={tab} className={`flex-1 px-4 py-2 text-sm font-medium rounded-lg transition-colors cursor-pointer ${activeTab === tab ? "bg-indigo-600 text-white" : "text-gray-600 hover:text-gray-900"}`}>
+                {tab.charAt(0).toUpperCase() + tab.slice(1)}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        {/* Posts */}
+        {
+          activeTab === 'posts' && (
+            <div className='mt-6 flex flex-col items-center gap-6'>
+              {posts.map((post) => <PostCard key={post._id} post={post} />)}
+            </div>
+          )
+        }
       </div>
     </div>
   ) : (<Loading/>)
