43050745cc5134547ddcfef008ed27b989aff32e
harsh <harshguptahb055@gmail.com>
2026-01-02 20:49:46 +0530

    fixed bugs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,12 +8,12 @@ export const getUserData = async (req, res) => {
         const { userId } = req.auth();
         const user = await User.findById(userId);
         if(!user){
-            return res.json({success: false, message: "User not found!"});
+            return res.status(404).json({success: false, message: "User not found!"});
         }
         res.json({success: true, user});
     } catch (error) {
         console.log(error);
-        res.json({success: false, message: error.message});
+        res.status(500).json({success: false, message: "User not found"});
     }
 }
 
@@ -21,21 +21,29 @@ export const getUserData = async (req, res) => {
 export const updateUserData = async (req, res) => {
     try {
         const { userId } = req.auth();
-        const {username, bio, location, full_name} = req.body;
+        let {username, bio, location, full_name} = req.body;
+
         const tempUser = await User.findById(userId);
+        if(!tempUser){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
         !username && (username = tempUser.username); //if username is not provided, add already existing username
         if(tempUser.username !== username){
-            const user = User.findOne({username});
+            const user = await User.findOne({username});
             if(user){
                 //we will not change the username if it is already taken
                 username = tempUser.username;
             }
         }
         const updatedData = {
-            username,
-            bio,
-            location,
-            full_name
+            username: username || tempUser.username,
+            bio: bio || tempUser.bio,
+            location: location || tempUser.location,
+            full_name: full_name || tempUser.full_name
         }
 
         const profile = req.files.profile && req.files.profile[0];
@@ -51,7 +59,7 @@ export const updateUserData = async (req, res) => {
            const url = imagekit.url({
             path: response.filePath,
             transformation: [
-                {quality: auto},
+                {quality: 'auto'},
                 {format: 'webp'},
                 {width: '512'}
             ]
@@ -63,22 +71,23 @@ export const updateUserData = async (req, res) => {
            const buffer = fs.readFileSync(cover.path);
            const response = await imagekit.upload({
             file: buffer,
-            fileName: profile.originalname,
+            fileName: cover.originalname,
            });
 
            const url = imagekit.url({
             path: response.filePath,
             transformation: [
-                {quality: auto},
+                {quality: 'auto'},
                 {format: 'webp'},
                 {width: '1280'}
             ]
            });
            updatedData.cover_photo = url;
         }
-        const user = await User.findByIdAndUpdate(userId, updateUserData, {new: true}); //{new: true}, so it will return after updating the user
+        const user = await User.findByIdAndUpdate(userId, updatedData, {new: true}); //{new: true}, so it will return after updating the user
         res.json({success: true, user, message: "Profile updated successfully"})
     } catch (error) {
         console.log(error.message);
+        res.status(500).json({sucess: false, message: error.message});
     }
 }
