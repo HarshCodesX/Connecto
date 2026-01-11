e14ec976abc6077baa572276059822c35bb2c832
harsh <harshguptahb055@gmail.com>
2026-01-12 02:40:57 +0530

    implemented mail function to send email

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -2,18 +2,18 @@ import nodemailer from "nodemailer";
 
 // Create a transporter object using the SMTP settings
 const transporter = nodemailer.createTransport({
-    host: "",
+    host: "smtp-relay.brevo.com",
     PORT: 587,
     sccure: false, //true for 465, false for other ports
     auth: {
-        user: "",
-        pass: ""
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     },
 });
 
 const sendEmail = async ({to, subject, body}) => {
     const response = transporter.sendMail({
-        from: "",
+        from: process.env.SENDER_EMAIL,
         to,
         subject,
         html: body,
