a648fe4676da1006319f9da0cb6b1a39fe297ccb
harsh <harshguptahb055@gmail.com>
2026-01-11 19:36:58 +0530

    implemented getConnections, acceptConnection and sendConection controllers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -221,3 +221,33 @@ export const getUserConnections = async (req, res) => {
         res.status(400).json({success: false, message: error.message});
     }
 }
+
+// Accept Connection Request
+export const acceptConnectionRequest = async (req, res) => {
+    try {
+        const { userId } = req.auth();
+        const { id } = req.body;
+
+        const connection = await Connection.findOne({from_user_id: id, to_user_id: userId});
+
+        if(!connection){
+            return res.json({success: false, message: 'Connection not found'});
+        }
+
+        const user = await User.findById(userId);
+        user.connections.push(id);
+        await user.save();
+
+        const toUser = await User.findById(id);
+        toUser.connections.push(userId);
+        await toUser.save();
+
+        connection.status = 'accepted';
+        await connection.save();
+
+        res.json({success: true, message: 'Connection accepted successfully'});
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({success: false, message: error.message});
+    }
+}
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { discoverUsers, followUser, getUserData, unfollowUser, updateUserData } from "../controllers/userController.js";
+import { acceptConnectionRequest, discoverUsers, followUser, getUserConnections, getUserData, sendConnectionRequest, unfollowUser, updateUserData } from "../controllers/userController.js";
 import { protect } from "../middlewares/auth.js";
 import { upload } from "../configs/multer.js";
 
@@ -7,8 +7,11 @@ const userRouter = express.Router();
 
 userRouter.get('/data', protect, getUserData);
 userRouter.post('/update', upload.fields([{name: 'profile', maxCount: 1}, {name: 'cover', maxCount: 1}]), protect, updateUserData);
-userRouter.get('/discover', protect, discoverUsers);
-userRouter.get('/follow', protect, followUser);
-userRouter.get('/unfollow', protect, unfollowUser);
+userRouter.post('/discover', protect, discoverUsers);
+userRouter.post('/follow', protect, followUser);
+userRouter.post('/unfollow', protect, unfollowUser);
+userRouter.post('/connect', protect, sendConnectionRequest);
+userRouter.post('/accept', protect, acceptConnectionRequest);
+userRouter.get('/connections', protect, getUserConnections);
 
 export default userRouter;
