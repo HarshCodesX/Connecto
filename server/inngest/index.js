51c9d4bdb706a7dbe412c0c6bfad4669969401cd
harsh <harshguptahb055@gmail.com>
2026-01-12 14:22:51 +0530

    removed unnecessary line

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,6 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Connection from "../models/Connection.js";
 import sendEmail from "../configs/nodeMailer.js";
-import { connection } from "mongoose";
 
 //Create a client to send and recover events
 export const inngest = new Inngest({id: "pingup-app"});
