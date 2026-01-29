641dbbfb489254ce642685e1f3091ecc80b05eca
harsh <harshguptahb055@gmail.com>
2026-01-29 20:09:09 +0530

    continued FE and BE integration

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,17 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { dummyPostsData } from '../assets/assets';
+import { useEffect, useState } from 'react'
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
 import PostCard from '../components/PostCard';
 import {assets} from '../assets/assets.js';
 import RecentMessages from '../components/RecentMessages.jsx';
+import { useAuth } from '@clerk/clerk-react';
+import api from '../api/axios.js';
+import toast from 'react-hot-toast';
 
 const Feed = () => {
-  const [feeds, setfeeds] = useState([]);
+  const [feeds, setFeeds] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { getToken } = useAuth();
 
   const fetchFeeds = async () => {
-    setfeeds(dummyPostsData);
+    try {
+      setLoading(true);
+      const { data } = await api.get('/api/post/feed', {headers: {Authorization: `Bearer ${await getToken()}`}});
+      if(data.success){
+        setFeeds(data.posts);
+      }
+      else{
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
     setLoading(false);
   }
   useEffect(() => {
